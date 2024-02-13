a512c0167ad8102a0407e988a81e7f88b484270e
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2024-02-12 21:15:11 -0300

    docs(core): Adding some comments in the code for better understanding

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ from dotenv import load_dotenv
 
 from utils import authenticate, get_spreadsheet
 
-load_dotenv()
 
-client = authenticate(
-    ["https://www.googleapis.com/auth/spreadsheets"],
-)
+def main():
+    load_dotenv()  # Load environment variables from .env file
 
-spreadsheet = get_spreadsheet(client, os.getenv("SPREADSHEET_ID"))
+    if not os.getenv("SPREADSHEET_ID"):
+        raise EnvironmentError("SPREADSHEET_ID not found in .env file")
 
-worksheet = spreadsheet.sheet1
+    try:
+        client = authenticate(
+            ["https://www.googleapis.com/auth/spreadsheets"],
+        )
+    except Exception as e:
+        print(f"Error authenticating with Google Sheets API: {e}")
+        return
 
-# worksheet.update_title("A Spreadsheet")
+    spreadsheet = get_spreadsheet(client, os.getenv("SPREADSHEET_ID"))
 
-# worksheet.update_cell(1, 1, "Hello World!")
+    worksheet = spreadsheet.sheet1
 
-# acess_value = worksheet.cell(1, 1).value
+    # Some examples of what you can do with the worksheet
+    # worksheet.update_title("A Spreadsheet")
+    # worksheet.update_cell(1, 1, "Hello World!")
+    # acess_value = worksheet.cell(1, 1).value
+    # find_value = worksheet.find("Hello World!")
+    # worksheet.format("A1", {"textFormat": {"bold": True}})
 
-# find_value = worksheet.find("Hello World!")
 
-# worksheet.format("A1", {"textFormat": {"bold": True}})
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,12 +1,33 @@
 from google.oauth2.service_account import Credentials
 import gspread
 
+
 def authenticate(scopes: list[str]) -> gspread.Client:
+    """Authenticate user with Google Sheets API using service accoun credentials
+
+    Args:
+        `scopes (list[str])`: List of api scopes to authenticate with
+
+    Returns:
+        `gspread.Client`: Authenticate client ready to acess Google Sheets API
+    """
+
     credentials = Credentials.from_service_account_file(
         "credentials.json", scopes=scopes
     )
 
     return gspread.authorize(credentials)
 
-def get_spreadsheet(client: gspread.Client, sheet_id: str) -> gspread.Spreadsheet:
-    return client.open_by_key(sheet_id)
+
+def get_spreadsheet(client: gspread.Client, spreadsheet_id: str) -> gspread.Spreadsheet:
+    """Get a Google Sheets spreadsheet by its id
+
+    Args:
+        `client (gspread.Client)`: Authenticated client object
+        `spreadsheet_id (str)`: Id of the spreadsheet to acess
+
+    Returns:
+        `gspread.Spreadsheet`: The spreadsheet object accesed by the id
+    """
+
+    return client.open_by_key(spreadsheet_id)
